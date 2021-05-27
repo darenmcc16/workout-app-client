bde93dad1a7e98538ddabb2762c0150c6614e883
Daren McCarrell <darenmcc16@gmail.com>
2021-05-27 18:02:08 -0500

    deploy config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
   
-export const API_ENDPOINT= "http://localhost:8000/api";
+//export const API_ENDPOINT= "http://localhost:8000/api";
 
-//export const API_ENDPOINT="https://restaurant-finder-serv.herokuapp.com/api"
+export const API_ENDPOINT="https://gym-hero-thinkful.herokuapp.com/api"
 
 export const TOKEN_KEY= 'AIzaSyDpDwx_xK2tomJ1kPgFhl8tR1YVtfcWiJ0';
 export const USER_TOKEN_KEY="gym-hero-key";
